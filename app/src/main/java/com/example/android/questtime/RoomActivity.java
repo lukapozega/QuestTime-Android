c3379a9ac30123bdd3ebb594f483c27feb54ad98
Filip Grebenac <>
2018-05-04 09:05:06 +0200

    Added people activity files

--- app/src/main/java/com/example/android/questtime/RoomActivity.java
+++ app/src/main/java/com/example/android/questtime/RoomActivity.java
@@ -1,8 +1,13 @@
 package com.example.android.questtime;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,10 +19,14 @@ public class RoomActivity extends AppCompatActivity {
     ArrayList<String> questions = new ArrayList<>();
     ArrayAdapter<String> adapter;
 
+    ImageView peopleButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.room_activity);
+
+        peopleButton = (ImageView) findViewById(R.id.peopleBtn);
 
         questions.add(randomQuestion);
         questions.add(randomQuestion);
@@ -27,6 +36,14 @@ public class RoomActivity extends AppCompatActivity {
         questionsList = findViewById(R.id.questions_list_view);
         adapter = new QuestionAdapter(this, questions);
         questionsList.setAdapter(adapter);
+
+        peopleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RoomActivity.this, PeopleActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
